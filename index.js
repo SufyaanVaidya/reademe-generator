b23080104f319ebfb78ff1af8d8f5f71bcae67a5
Sufyaan Vaidya <vaidyasufyaan@gmail.com>
2022-07-13 23:23:24 -0400

    added packages

--- index.js
+++ index.js
@@ -1,4 +1,7 @@
 // TODO: Include packages needed for this application
+const fs = require('fs');
+const inquirer = require('inquirer');
+const util = require('util');
 
 // TODO: Create an array of questions for user input
 const questions = [
