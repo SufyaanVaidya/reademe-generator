a719616ba2ec2ff1296b1a01e4b16031edfe4eed
Sufyaan Vaidya <vaidyasufyaan@gmail.com>
2022-07-13 23:12:18 -0400

    added questions

---
+++ index.js
@@ -0,0 +1,19 @@
+// TODO: Include packages needed for this application
+
+// TODO: Create an array of questions for user input
+const questions = [
+    title = "What's the name of your repo?",
+    description = "How would you describe your application?",
+    license = "Which license would you like to use?",
+    userName = "Please enter GitHub Username?",
+    email = "Please enter your email-"
+];
+
+// TODO: Create a function to write README file
+function writeToFile(fileName, data) {}
+
+// TODO: Create a function to initialize app
+function init() {}
+
+// Function call to initialize app
+init();
